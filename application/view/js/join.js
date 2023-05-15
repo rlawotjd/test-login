843c3d47d6c260e3b0713573a818339cf0f7a3aa
rlawotjd <rlawotjd026@gmail.com>
2023-05-15 18:11:56 +0900

    5.15이것저것

---
+++ application/view/js/join.js
@@ -0,0 +1,7 @@
+const idCheck = document.getElementById('id');
+const pwCheck = document.getElementById('pw');
+const pw_cCheck = document.getElementById('pw_c');
+const id_c=document.getElementById('id_c')
+if (idCheck.value.length>=6&&pwCheck.value.length>=8&&pwCheck.value===pw_cCheck.value) {
+    
+}
